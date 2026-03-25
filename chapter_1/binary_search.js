5ff8e54175f93ddda69205fb8f23bbcd549a7e8e
Augusto Yunes <augusto.yunes@accenture.com>
2026-03-25 18:50:39 -0300

    adding search for first and last occurrences

--- chapter_1/binary_search.js
+++ chapter_1/binary_search.js
@@ -14,4 +14,41 @@ function binarySearch (array, target) {
   return -1
 }
 
-export { binarySearch }
+function binarySearchFirstOcurrence (array, target) {
+  let low = 0;
+  let high = array.length -1;
+  let result = -1;
+  while (low <= high) {
+    const middle = ((low + high) / 2);
+    const guess = array[middle];
+    if (guess === target) {
+      result = middle;
+      high = middle - 1;
+    } else if (guess > target) {
+      high = middle - 1;
+    } else {
+      low = middle + 1
+    }
+  }
+  return result;
+}
+
+function binarySearchLastOcurrence (array, target) {
+  let low = 0;
+  let high = array.length - 1;
+  let result = -1;
+  while (low <= high) {
+    const middle = Math.floor((low + high) / 2);
+    const guess = array[middle];
+    if (guess === target) {
+      result = middle;
+      low = middle + 1;
+    } else if (guess > target) {
+      high = middle - 1;
+    } else {
+      low = middle + 1
+    }
+  }
+  return result;
+}
+export { binarySearch, binarySearchFirstOcurrence, binarySearchLastOcurrence }
--- chapter_1/binary_search.spec.js
+++ chapter_1/binary_search.spec.js
@@ -1,14 +1,37 @@
-import { binarySearch } from "./binary_search";
+import { binarySearch, binarySearchFirstOcurrence, binarySearchLastOcurrence } from "./binary_search";
 
-const mockArray = [1, 3, 5, 7, 9]
+const mockArray = [1, 3, 5, 7, 9];
+const mockArrayMultiOcurrences = [1, 2, 2, 2, 3, 5, 7, 9];
 
-describe('binary search tests', () => {
-  it('should find result in slot 1', () => {
+describe("binary search tests", () => {
+  it("should find result in slot 1", () => {
     const result = binarySearch(mockArray, 3);
-    expect(result).toEqual(1)
-  })
-  it('should return -1 when result is not found', () => {
+    expect(result).toEqual(1);
+  });
+  it("should return -1 when result is not found", () => {
     const result = binarySearch(mockArray, 4);
-    expect(result).toEqual(-1)
-  })
-})
+    expect(result).toEqual(-1);
+  });
+});
+
+describe("binary search first occurrence tests", () => {
+  it("should find result in position 1", () => {
+    const result = binarySearchFirstOcurrence(mockArrayMultiOcurrences, 2);
+    expect(result).toEqual(1);
+  });
+  it("should return -1 when result is not found", () => {
+    const result = binarySearchFirstOcurrence(mockArrayMultiOcurrences, 4);
+    expect(result).toEqual(-1);
+  });
+});
+
+describe("binary search last occurrence tests", () => {
+  it("should find result in position 1", () => {
+    const result = binarySearchLastOcurrence(mockArrayMultiOcurrences, 2);
+    expect(result).toEqual(3);
+  });
+  it("should return -1 when result is not found", () => {
+    const result = binarySearchLastOcurrence(mockArrayMultiOcurrences, 4);
+    expect(result).toEqual(-1);
+  });
+});
